1f756d8558a72fb667e28e398adeff9f0b4f903a
Mohammad Mohibul Hasan <mhpeash.ewu@hotmail.com>
2016-10-13 21:05:26 +0600

    Add Project FirstTime

---
+++ ProjectDentist/ProjectDentist/DentistDBContext/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ProjectDentist.Models;
+
+
+namespace ProjectDentist.DentistDBContext
+{
+    public class DBContext:DbContext
+
+    {
+        public DbSet<Registration> Registrations { get; set; }
+    }
+}
---
+++ ProjectDentist/ProjectDentist/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectDentist.MvcApplication" Language="C#" %>
