88bd1520d7f93e4958d22c2fe05649234478c491
Mohammad Mohibul Hasan <mhpeash.ewu@hotmail.com>
2016-10-16 20:47:04 +0600

    Working with Registration Model

---
+++ Project Dentist/DentistProject/DentistProject/Context/DentistContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using DentistProject.Models;
+
+namespace DentistProject.Context
+{
+    public class DentistContext:DbContext
+    {
+        public DbSet<Registration> Registration { get; set; }
+
+    }
+}
